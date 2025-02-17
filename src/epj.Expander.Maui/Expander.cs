8dd56053caf5627e2f0bbc218e50958e8ab7c84b
Graňák Pavel <pavel.granak@gmail.com>
2025-02-17 14:14:47 +0100

    Update Expander.cs
    Change expander header to text part and image in right part. Added new bindable properties:
    HeaderTextProperty
    HeaderBorderStyleProperty
    HeaderTextStyleProperty
    HeaderIconStyleProperty
    HeaderIconDefaultRotationAngleProperty
    HeaderIconRotateAngleProperty

--- src/epj.Expander.Maui/Expander.cs
+++ src/epj.Expander.Maui/Expander.cs
@@ -1,6 +1,5 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Windows.Input;
-
 namespace epj.Expander.Maui;
 
 #pragma warning disable CA1001
@@ -10,10 +9,32 @@ public class Expander : ContentView
     private static bool _animationsEnabled;
 
     private readonly SemaphoreSlim _semaphoreSlim = new(1, 1);
-
-    private Grid HeaderGrid { get; }
+   
+    private Grid HeaderGrid { get; } 
     private Grid BodyGrid { get; }
+    private Border MyHeaderBorder { get; set; } 
+    private Label MyHeaderText { get; set; }
+    private Label MyHeaderIcon { get; }
 
+    //propertyChanged: OnHeaderTextPropertyChanged
+    public static readonly BindableProperty HeaderTextProperty = BindableProperty.Create(nameof(HeaderText), typeof(string), typeof(Expander), "", BindingMode.TwoWay);
+    public static readonly BindableProperty HeaderBorderStyleProperty = BindableProperty.CreateAttached(nameof(HeaderBorderStyle), typeof(Style), typeof(Border), default(Style), BindingMode.OneWay);
+    public static readonly BindableProperty HeaderTextStyleProperty = BindableProperty.CreateAttached(nameof(HeaderTextStyle), typeof(Style), typeof(Label), default(Style), BindingMode.OneWay);
+    public static readonly BindableProperty HeaderIconStyleProperty = BindableProperty.CreateAttached(nameof(HeaderIconStyle), typeof(Style), typeof(Label), default(Style), BindingMode.OneWay);
+    public static readonly BindableProperty HeaderIconDefaultRotationAngleProperty = BindableProperty.CreateAttached(nameof(HeaderIconDefaultRotationAngle), typeof(Double), typeof(Expander), 0D, BindingMode.TwoWay);
+    public static readonly BindableProperty HeaderIconRotateAngleProperty = BindableProperty.CreateAttached(nameof(HeaderIconRotateAngle), typeof(Double), typeof(Expander), -90D, BindingMode.TwoWay);
+
+
+    public static readonly BindableProperty IsExpandedProperty = BindableProperty.Create(nameof(IsExpanded), typeof(bool), typeof(Expander), propertyChanged: OnIsExpandedPropertyChanged);
+    public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(Expander));
+    public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(Expander));
+    public static readonly BindableProperty HeaderContentProperty = BindableProperty.Create(nameof(HeaderContent), typeof(View), typeof(Expander), propertyChanged: OnHeaderContentPropertyChanged);
+    public static readonly BindableProperty BodyContentProperty = BindableProperty.Create(nameof(BodyContent), typeof(View), typeof(Expander), propertyChanged: OnContentPropertyChanged);
+    public static readonly BindableProperty ExpandEasingProperty = BindableProperty.Create(nameof(ExpandEasing), typeof(Easing), typeof(Expander), defaultValue: Easing.CubicIn);
+    public static readonly BindableProperty CollapseEasingProperty = BindableProperty.Create(nameof(CollapseEasing), typeof(Easing), typeof(Expander), defaultValue: Easing.CubicOut);
+    public static readonly BindableProperty ExpandDurationProperty = BindableProperty.Create(nameof(ExpandDuration), typeof(int), typeof(Expander), defaultValue: 250, propertyChanged: OnExpandDurationPropertyChanged);
+    public static readonly BindableProperty CollapseDurationProperty = BindableProperty.Create(nameof(CollapseDuration), typeof(int), typeof(Expander), defaultValue: 250, propertyChanged: OnCollapseDurationPropertyChanged);
+       
     /// <summary>
     /// Use to set the header content of the expander
     /// </summary>
@@ -106,15 +127,57 @@ public class Expander : ContentView
         }
     }
 
-    public static readonly BindableProperty IsExpandedProperty = BindableProperty.Create(nameof(IsExpanded), typeof(bool), typeof(Expander), propertyChanged: OnIsExpandedPropertyChanged);
-    public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(Expander));
-    public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(Expander));
-    public static readonly BindableProperty HeaderContentProperty = BindableProperty.Create(nameof(HeaderContent), typeof(View), typeof(Expander), propertyChanged: OnHeaderContentPropertyChanged);
-    public static readonly BindableProperty BodyContentProperty = BindableProperty.Create(nameof(BodyContent), typeof(View), typeof(Expander), propertyChanged: OnContentPropertyChanged);
-    public static readonly BindableProperty ExpandEasingProperty = BindableProperty.Create(nameof(ExpandEasing), typeof(Easing), typeof(Expander), defaultValue: Easing.CubicIn);
-    public static readonly BindableProperty CollapseEasingProperty = BindableProperty.Create(nameof(CollapseEasing), typeof(Easing), typeof(Expander), defaultValue: Easing.CubicOut);
-    public static readonly BindableProperty ExpandDurationProperty = BindableProperty.Create(nameof(ExpandDuration), typeof(int), typeof(Expander), defaultValue: 250, propertyChanged: OnExpandDurationPropertyChanged);
-    public static readonly BindableProperty CollapseDurationProperty = BindableProperty.Create(nameof(CollapseDuration), typeof(int), typeof(Expander), defaultValue: 250, propertyChanged: OnCollapseDurationPropertyChanged);
+    public string HeaderText
+    {
+        get => (string)GetValue(HeaderTextProperty);
+        set => SetValue(HeaderTextProperty, value);
+    }
+
+    public Style HeaderBorderStyle
+    {
+        get => (Style)GetValue(HeaderBorderStyleProperty);
+        set => SetValue(HeaderBorderStyleProperty, value);
+    }
+
+    public Style HeaderTextStyle
+    {
+        get => (Style)GetValue(HeaderTextStyleProperty);
+        set => SetValue(HeaderTextStyleProperty, value);
+    }
+    public Style HeaderIconStyle
+    {
+        get => (Style)GetValue(HeaderIconStyleProperty);
+        set => SetValue(HeaderIconStyleProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the header icon default image rotation angle.
+    /// </summary>
+    /// <value>
+    /// 0D
+    /// </value>
+    public Double HeaderIconDefaultRotationAngle
+    { 
+        get => (Double)GetValue(HeaderIconDefaultRotationAngleProperty);
+        set => SetValue(HeaderIconDefaultRotationAngleProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets the header icon rotate image angle.
+    /// </summary>
+    /// <value>
+    /// -90D
+    /// </value>
+    public Double HeaderIconRotateAngle
+    {
+        get => (Double)GetValue(HeaderIconRotateAngleProperty);
+        set => SetValue(HeaderIconRotateAngleProperty, value);
+    }
+
+    private static void OnHeaderTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        ((Label)bindable).Text = (string)newValue;
+    }
 
     private static void OnIsExpandedPropertyChanged(BindableObject bindable, object oldValue, object newValue)
     {
@@ -186,16 +249,37 @@ public class Expander : ContentView
 
     public Expander()
     {
+        MyHeaderBorder = new Border()
+        {
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Fill,
+        };
+
+        MyHeaderText = new Label
+        {
+            Text = "Default text:"
+        };
+
+        MyHeaderIcon = new Label
+        { 
+            Rotation = HeaderIconDefaultRotationAngle 
+        };
+
         HeaderGrid = new Grid
         {
             ZIndex = 500,
             HorizontalOptions = LayoutOptions.Fill,
-            VerticalOptions = LayoutOptions.Start
+            VerticalOptions = LayoutOptions.Center,
+            RowDefinitions = { new RowDefinition { Height = GridLength.Star } },
+            ColumnDefinitions = { new ColumnDefinition { Width = GridLength.Star }, new ColumnDefinition { Width = new GridLength(30) } },
         };
 
         var tapGestureRecognizer = new TapGestureRecognizer();
         tapGestureRecognizer.Tapped += OnHeaderContentTapped;
         HeaderGrid.GestureRecognizers.Add(tapGestureRecognizer);
+        MyHeaderBorder.GestureRecognizers.Add(tapGestureRecognizer);
+        MyHeaderText.GestureRecognizers.Add(tapGestureRecognizer);
+        MyHeaderIcon.GestureRecognizers.Add(tapGestureRecognizer);
 
         BodyGrid = new Grid
         {
@@ -205,22 +289,27 @@ public class Expander : ContentView
             IsClippedToBounds = true
         };
 
+        MyHeaderBorder.SetBinding(Border.StyleProperty, new Binding(nameof(HeaderBorderStyle), source: this));
         BodyGrid.SetBinding(Grid.IsVisibleProperty, new Binding(nameof(IsExpanded), source: this));
+        MyHeaderText.SetBinding(Label.TextProperty, new Binding(nameof(HeaderText),  source: this));
+        MyHeaderText.SetBinding(Label.StyleProperty, new Binding(nameof(HeaderTextStyle), source: this));
+        MyHeaderIcon.SetBinding(Label.StyleProperty, new Binding(nameof(HeaderIconStyle), source: this));
 
         var expanderGrid = new Grid
         {
             RowDefinitions =
             {
-                new RowDefinition(GridLength.Auto),
-                new RowDefinition(GridLength.Auto),
+                new RowDefinition { Height = GridLength.Star },
+                new RowDefinition(GridLength.Star),
             }
         };
 
-        expanderGrid.Add(HeaderGrid);
-        expanderGrid.SetRow(HeaderGrid, 0);
+        HeaderGrid.Add(MyHeaderText, 0,0);
+        HeaderGrid.Add(MyHeaderIcon, 1, 0);
+        MyHeaderBorder.Content = HeaderGrid;
 
-        expanderGrid.Add(BodyGrid);
-        expanderGrid.SetRow(BodyGrid, 1);
+        expanderGrid.Add(MyHeaderBorder, 0,0);
+        expanderGrid.Add(BodyGrid,0,1);
 
         Content = expanderGrid;
     }
@@ -263,6 +352,9 @@ public class Expander : ContentView
                 var animation = new Animation()
                     .Add(BodyContent.AnimateHeightRequest(start: 0, end: size.Minimum.Height, easing: ExpandEasing))
                     .Add(BodyContent.AnimateTranslationY(start: -size.Minimum.Height, end: 0, easing: ExpandEasing));
+
+                MyHeaderIcon.Rotation = HeaderIconRotateAngle;
+
                 await BodyContent.AnimateAsync(animation, ExpandDuration);
             }
             else
@@ -270,6 +362,9 @@ public class Expander : ContentView
                 var animation = new Animation()
                     .Add(BodyContent.AnimateHeightRequest(start: size.Minimum.Height, end: 0, easing: CollapseEasing))
                     .Add(BodyContent.AnimateTranslationY(start: 0, end: -size.Minimum.Height, easing: CollapseEasing));
+
+                MyHeaderIcon.Rotation = HeaderIconDefaultRotationAngle;
+
                 await BodyContent.AnimateAsync(animation, CollapseDuration);
 
                 OnPropertyChanged(nameof(IsExpanded));
